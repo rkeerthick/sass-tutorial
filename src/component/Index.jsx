dfea4e875488de08dc71dcbeaab4308592a57bae
rkeerthick <rkeerthick1@gmail.com>
2023-09-22 14:53:32 +0530

    22/9/23 first commit

---
+++ src/component/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nam quas. Facere dicta porro aspernatur repudiandae, neque expedita fuga culpa quas impedit possimus.</p>
+    </div>
+  )
+}
+
+export default Index
